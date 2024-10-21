ed886dc36c78a013e393dac10702e388985d13f8
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-10-21 08:18:08 +0200

    UK-5: Add env settings

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export * from '../assets/config/firebase-dev.config';
+
+export const IS_PROD = false;
+export const ENV_NAME = 'DEV';
+export const API_URL = 'http://127.0.0.1:5001/u-kirche-stage/us-central1/api';
