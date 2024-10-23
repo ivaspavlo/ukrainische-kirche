0c46d2e27e72dd4d015e9c55c841bc7c59532050
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-10-23 08:28:15 +0200

    UK-6.1: Adjust hosting configuration

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,13 @@
-export * from '../assets/config/firebase-stage.config';
-
 export const IS_PROD = false;
 export const ENV_NAME = 'STAGE';
 export const API_URL = 'https://api-dnmmpquwoq-uc.a.run.app';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCyffN1tCxqMl5RDMGtfb0jJARt91XCP9c',
+  authDomain: 'u-kirche-stage.firebaseapp.com',
+  projectId: 'u-kirche-stage',
+  storageBucket: 'u-kirche-stage.appspot.com',
+  messagingSenderId: '826406220808',
+  appId: '1:826406220808:web:52126eacb5ba7a427c0dbd',
+  measurementId: 'G-F0H9K6JECQ'
+};
