a633fa1fe00a2b9b408980224d30e916d04f8d23
Pavlo Ivashchenko <39334589+ivaspavlo@users.noreply.github.com>
2024-10-23 08:45:16 +0200

    UK-6.1/6.2: Adjust firebase settings (#7)
    * UK-6.1: Adjust hosting configuration
    
    * UK-6.2: Temporary remove apiKey
    
    ---------
    
    Co-authored-by: Pavel Ivashchenko <pavel.ivashchenko@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
-export * from '../assets/config/firebase-prod.config';
-
 export const IS_PROD = true;
 export const ENV_NAME = 'PROD';
-export const API_URL = '';
+export const API_URL = 'https://api-dnmmpquwoq-uc.a.run.app';
+
+export const firebaseConfig = {};
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
-export * from '../assets/config/firebase-stage.config';
-
 export const IS_PROD = false;
 export const ENV_NAME = 'STAGE';
 export const API_URL = 'https://api-dnmmpquwoq-uc.a.run.app';
+
+export const firebaseConfig = {};
