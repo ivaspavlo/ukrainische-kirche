01be93fb18732d09826063dbfbe741eb55d42110
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-10-26 08:54:38 +0200

    UK-6.3: Return apiKey after investigation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,12 @@ export const IS_PROD = true;
 export const ENV_NAME = 'PROD';
 export const API_URL = 'https://api-dnmmpquwoq-uc.a.run.app';
 
-export const firebaseConfig = {};
+export const firebaseConfig = {
+  apiKey: 'AIzaSyACllZ7MUk8xEFNrX0gx9O-B3hJ1zWNyuU',
+  authDomain: 'u-kirche-prod.firebaseapp.com',
+  projectId: 'u-kirche-prod',
+  storageBucket: 'u-kirche-prod.appspot.com',
+  messagingSenderId: '797540027619',
+  appId: '1:797540027619:web:01f790f66efb00560dc9d6',
+  measurementId: 'G-RP1C52RSJZ'
+};
