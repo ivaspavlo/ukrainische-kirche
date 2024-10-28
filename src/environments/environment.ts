1e69ea189f9bfc1c8b38598c22357def0035e95d
Pavlo Ivashchenko <39334589+ivaspavlo@users.noreply.github.com>
2024-10-28 08:40:18 +0100

    UK-5/6: Implement deploy config (#8)
    * UK-5: Add env settings (#5)
    
    Co-authored-by: Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
    
    * UK-5/6: Configure deployment files (#6)
    
    * UK-5: Add env settings
    
    * UK-6: Configure hosting deployment files
    
    ---------
    
    Co-authored-by: Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
    
    * UK-6.1: Adjust hosting configuration
    
    * UK-6.2: Temporary remove apiKey
    
    * UK-6.3: Return apiKey after investigation
    
    * UK-6.4: Fix deploy error
    
    ---------
    
    Co-authored-by: Pavel Ivashchenko <pavel.ivashchenko@gmail.com>

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const IS_PROD = false;
+export const ENV_NAME = 'DEV';
+export const API_URL = 'https://api-7bejm676fa-uc.a.run.app';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmufuELEUUAGT2D2ynfVvLuAtMv-OSRJ8",
+  authDomain: "u-kirche-dev.firebaseapp.com",
+  projectId: "u-kirche-dev",
+  storageBucket: "u-kirche-dev.appspot.com",
+  messagingSenderId: "1084931506587",
+  appId: "1:1084931506587:web:6b6138c02d05d2d0baa13e",
+  measurementId: "G-FF9EM4EDFL"
+};
