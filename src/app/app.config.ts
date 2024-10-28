84ab8a5283ad4bc502427120234e3d573e3804fd
Pavel Ivashchenko <pavel.ivashchenko@gmail.com>
2024-10-28 08:15:35 +0100

    UK-6.4: Fix deploy error

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,7 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-
-import { firebaseConfig } from '@env/environment';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-getAnalytics(app);
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+
+import { firebaseConfig } from '@env/environment';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
